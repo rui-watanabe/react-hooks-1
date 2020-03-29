f5e1bbf6bfd57be83d135c1ed0423c5bf8d5f77f
rui-watanabe <wataru610806@gmail.com>
2020-03-29 14:49:30 +0900

    delete all operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -37,12 +37,23 @@ const EventForm = () => {
     })
 
     dispatch({
-      type: DELETE_ALL_OPERATION_LOGS,
+      type: ADD_OPERATION_LOG,
       description: '全てのイベントを削除しました。',
       operatedAt: timeCurrentIso8601()
     })
     
   }
+
+  const deleteAllOperationLogs = e => {
+    e.preventDefault()
+    const result = window.confirm('全ての操作ログを本当に削除してもいいですか?')
+    if(result) {
+      dispatch({
+        type: DELETE_ALL_OPERATION_LOGS
+      })
+    }
+  }
+
   return(
     <>
       <h4>イベント作成フォーム</h4>
@@ -59,6 +70,7 @@ const EventForm = () => {
 
         <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
         <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
+        <button className="btn btn-danger" onClick={deleteAllOperationLogs} disabled={state.operationLogs.length === 0}>全ての操作ログを削除する</button>
       </form>
     </>
   )
